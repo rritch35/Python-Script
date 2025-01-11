a5ee836cadf92a8335b50c0abb334119215a87fc
rritch35 <rritch35@gmail.com>
2025-01-11 14:54:51 -0500

    first commit testing the vs login

---
+++ quote.py
@@ -0,0 +1,8 @@
+#in this script I am going to proivde quotes, then going the python script will select a random quote.
+#  I will start with providing the quote then in the future hope to add an api implementation 
+
+import random #going to need random in order to select a random quote
+
+def get_quote()
+    quotes = [ ("Any sufficiently advanced technology is indistinguishable from magic."), ("Arthur C. Clarke")
+    ]
